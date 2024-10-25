0ff6371692cac64b7c0a7c230a0aa334870a8102
AliHasan06 <alihassanraja06@gmail.com>
2024-10-26 01:11:05 +0500

    done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    'bg-red-500',  
   ],
   theme: {
     extend: {
@@ -16,4 +17,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
